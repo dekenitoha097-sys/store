ea45c913822c557e31ec255b0505e9135456b0aa
Deken <dekenitoha097@gmail.com>
2025-08-27 23:47:58 +0100

    second commit

--- components/shop_component/Produict_detaille.tsx
+++ components/shop_component/Produict_detaille.tsx
@@ -1,4 +1,6 @@
-import { ShoppingCart,Eye } from 'lucide-react';
+import { ShoppingCart, Eye } from "lucide-react";
+import { useRef } from "react";
+import CardButton from "./Card_action";
 
 type Produit = {
   id: number;
@@ -11,16 +13,51 @@ type Produit = {
 };
 
 type Props = {
-  produit:Produit;
-}
+  produit: Produit;
+};
 
-const Produict_detaille = ({produit}:Props) => {
+const Produict_detaille = ({ produit }: Props) => {
+  const modalRef = useRef<HTMLDialogElement>(null);
+  const openModal = () => {
+    modalRef.current?.showModal();
+  };
+
   return (
     <div>
-      <button className="w-full text-white py-2 rounded-lg btn btn-primary transition">
+      <button
+        className="w-full text-white py-2 rounded-lg btn btn-primary transition"
+        onClick={openModal}
+      >
         <Eye />
         Voir le produit
       </button>
+
+
+
+      <dialog ref={modalRef} className="modal">
+        
+        <div className="modal-box space-y-4">
+          <div className="space-y-4">
+            <p className="font-bold text-2xl  pb-4">Non du produit : <span className="font-light text-lg text-info
+            ">
+              {produit.name}</span>
+            </p>
+            <img src={produit.image_url} alt="" className="w-full rounded-md" />
+            <p className="font-bold text-2xl">Description  du produit :<span className="font-light text-lg text-info"> {produit.description}</span></p>
+            <p  className="font-bold text-2xl">Prix du produit : <span className="font-light text-lg text-info">
+               {produit.price} MAD</span></p>
+              <CardButton produit={produit}/>
+          </div>
+
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn btn-secondary">Fermer</button>
+            </form>
+          </div>
+
+
+        </div>
+      </dialog>
     </div>
   );
 };
